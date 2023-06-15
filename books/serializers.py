23e9624388a1ed8ae68a1953c3c67af734c6f844
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-06-15 03:02:28 -0300

    user and books crud

--- books/serializers.py
+++ books/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from books.models import Book
 
@@ -6,3 +7,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'release_year', 'author_name', 'publisher', 'pages_quantity', 'created_at']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups', 'first_name']
--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from books.models import Book
+from books.serializers import BookSerializer, UserSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
